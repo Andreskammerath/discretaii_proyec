1ef2b1177abd8d325dc9395ffa2e02dc2cce4b6b
tupapiloaleman <andreskammerath@gmail.com>
2019-03-30 19:25:54 -0300

    trying to make input parser work

--- Rii.c
+++ Rii.c
@@ -3,33 +3,44 @@
 #include <stdbool.h>
 #include <assert.h>
 
-#define MAXS 256 //¿cuantos caracteres como maximo puede tener una linea? 
 
 int main()
 {
 	unsigned int n,m = 0;
-	char line[MAXS] = {0};
-	while(fgets (line, MAXS, stdin) != NULL) //module to ignore comments
+	int c = 0;
+	char edge[50] = {0};
+	int i = 0;
+	while(true) //module to ignore comments
 	{
-		char *p = line;
-		int len = strlen(line);
+		++i;
+		c = 0;
+		if(i == 1000) printf("se queda en el while");
+		c = fgetc(stdin);
+		if(c == EOF)
+			printf("no anda el fgetc");;
 
-		while (len > 0 && (line[len-1] == '\n' || line[len-1] == '\r'))
-            line[--len] = 0;    /* strip newline or carriage rtn    */
+		// int len = strlen(line);
+
+		// while (len > 0 && (line[len-1] == '\n' || line[len-1] == '\r'))
+  //           line[--len] = 0;    /* strip newline or carriage rtn    */
 
 
-        while (*p == ' ')    /* advance to first non-whitespace  */
-            p++;
+  //       while (*p == ' ')     advance to first non-whitespace  
+  //           p++;
 
-        /* skip lines beginning with 'c' or blank lines  */
-        if (*p == 'c' || !*p)
-            continue;
+  //       /* skip lines beginning with 'c' or blank lines  */
+  //       if (*p == 'c' || !*p)
+  //           continue;
 
-        if(*p == 'p')
-        	break;
+  //       if(*p == 'p')
+  //       	break;
 	}
 
-	printf("%s\n",line);
+	scanf("%s\n",edge);
+	scanf("%d\n",&n);
+	scanf("%d\n",&m);
+	printf("el valor de n es:%d\n", n);
+	printf("el valor de m es:%d\n", m);
 	//como crear un string char por char ó scanf los numeros n y m en el string line(ver mejor opción)
 	// while(true) //looking for n value
 	// {
