fcfd972d6c14e9bc74dcce554fe117607df883b8
marcos <marcoscerioni98@gmail.com>
2019-04-08 18:07:44 -0300

    quick sort works correctly, array of vertex works too, and we add same testcases

--- Rii.c
+++ Rii.c
@@ -1,7 +1,5 @@
 #include "Rii.h"
 
-#include <stdlib.h>
-
 void cambiar(u32* array, u32* array2, u32* array3, u32* array4)
 {
  	u32* a = array;
@@ -15,13 +13,54 @@ void cambiar(u32* array, u32* array2, u32* array3, u32* array4)
  	*b = *a;
  	*a = tmp;
 }
-// void quick_sort(u32* array, u32* array2, u32 com, u32 fin, u32* array3, u32* array4)
-// {
-// 	if(com >= fin)
-// 	{
-// 		return;
-// 	}
-// }
+void quick_sort(u32* array, u32 com, u32 fin, u32* array3)
+ {
+        // Base case: No need to sort arrays of length <= 1
+        if (com >= fin)
+        {
+            return;
+        }
+        // Choose pivot to be the last element in the subarray
+        u32 pivot = array[fin];
+
+        // Index indicating the "split" between elements smaller than pivot and 
+        // elements greater than pivot
+        u32 cnt = com;
+
+        // Traverse through array from l to r
+        for (u32 i = com; i <= fin; i++)
+        {
+            // If an element less than or equal to the pivot is found...
+            if (array[i] <= pivot)
+            {
+                // Then swap arr[cnt] and arr[i] so that the smaller element arr[i] 
+                // is to the left of all elements greater than pivot
+                cambiar(&array[cnt], &array[i], &array3[cnt], &array3[i]);
+
+                // Make sure to increment cnt so we can keep track of what to swap
+                // arr[i] with
+                cnt++;
+            }
+        }
+        // NOTE: cnt is currently at one plus the pivot's index 
+        // (Hence, the cnt-2 when recursively sorting the left side of pivot)
+        quick_sort(array, com, cnt-2, array3); // Recursively sort the left side of pivot
+        quick_sort(array, cnt, fin, array3);   // Recursively sort the right side of pivot
+}
+
+void copiarAVertice(u32* vertices, u32* vecinos,u32 m)
+{
+	u32 j = 0;
+	for (u32 i = 0; i < m; ++i)
+	{
+		if(vecinos[i] != vertices[j])
+		{
+			++j;
+			vertices[j] = vecinos[i];
+		}
+	}
+}
+
 Grafostv* ConstruccionDelGrafo()
 {
 	Grafostv* G = (Grafostv*)malloc(sizeof(Grafostv));
@@ -45,15 +84,17 @@ Grafostv* ConstruccionDelGrafo()
 	scanf("%d",&n);
 	scanf("%d",&m);
 	// printf("%u %u\n", n, m );
+	G->n = n;
 	G->color = (u32*)malloc(sizeof(u32) * n);
 	G->orden = (u32*)malloc(sizeof(u32) * n);
 	G->grado = (u32*)malloc(sizeof(u32) * n);
-	G->vecinos = (u32**)(malloc(sizeof(u32*) * 2));
-	for (int i = 0; i < 2; ++i)
+	G->vecinos = (u32**)malloc(sizeof(u32*) * 2);
+	G->vertices = (u32*)malloc(sizeof(u32*) * n);
+	for (u32 i = 0; i < 2; ++i)
 	{
 		G->vecinos[i] = (u32*)malloc(sizeof(u32)*2*m);
 	}
-	for (int i = 0; i < 2*m; i += 2)
+	for (u32 i = 0; i < 2*m; i += 2)
 	{	
 		if(scanf(" %c %u %u",&prev, &v, &w) != 3 || prev != 'e')
 		{
@@ -66,17 +107,26 @@ Grafostv* ConstruccionDelGrafo()
 		G->vecinos[1][i] = w;
 		G->vecinos[0][i+1] = w;
 		G->vecinos[1][i+1] = v;
-	}
-	cambiar(&G->vecinos[0][1],&G->vecinos[0][3],&G->vecinos[1][1],&G->vecinos[1][3]);
+	}	
+	quick_sort(G->vecinos[0], 0, 2*m-1, G->vecinos[1]);
+	G->vertices[0] = G->vecinos[0][0];
+	copiarAVertice(G->vertices,G->vecinos[0],2*m); 
+
 	return G;
 }
 int main()
 {
 	// begin of create graph
 	Grafostv* ptr = ConstruccionDelGrafo();
-	for (int j = 0; j < 14; ++j)
+	//u32 m = ptr->m;
+	/*for (int j = 0; j < 2*m; ++j)
 	{
 		printf("%u ------ %u\n",ptr->vecinos[0][j], ptr->vecinos[1][j] );
 	}
+	u32 n = ptr->n;
+	for (int i = 0; i < n; ++i)
+	{
+		printf("%u\n", ptr->vertices[i]);
+	}*/
 	return 0;
 }
--- Rii.h
+++ Rii.h
@@ -10,8 +10,14 @@ typedef unsigned int u32;
 typedef struct Grafost
 {
 	u32** vecinos;
+	u32* vertices;  // Posicion de los vertices
 	u32* color; //Structure type declarations do not support initializers
 	u32* grado;
 	u32* orden;
 	u32* ocupado;
+	u32 n;
 } Grafostv;
+
+void cambiar(u32* array, u32* array2, u32* array3, u32* array4);
+
+void quick_sort(u32* array, u32 com, u32 fin, u32* array3);
